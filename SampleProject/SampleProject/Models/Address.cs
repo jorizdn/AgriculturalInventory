c669f1e38d741e1964a3247f901494dba544db40
Joriz Nalanggay <joriz.dn@gmail.com>
2017-12-04 08:37:36 +0800

    Initial Commit

---
+++ SampleProject/SampleProject/Models/Address.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SampleProject.Models
+{
+    public partial class Address
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
